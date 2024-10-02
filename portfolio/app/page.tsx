d663ac620c98a83bb444772e695cf0bafcc6b00e
emanuela <emanuela.markovic@hotmail.com>
2024-10-02 20:23:49 +0200

    feature: Add Home component with theme toggle and font integration

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,33 +1,47 @@
 import Image from "next/image";
+import ThemeToggle from "../app/components/ThemeToggle";
+import { Montserrat } from "next/font/google";
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1
-          className="text-3xl sm:text-4xl text-center sm:text-left font-bold"
-          style={{ color: "var(--geist-foreground)" }}
-        >
-          Junior Webdeveloper
-        </h1>
-        <ol className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-          <li className="mb-2">
-            "Code is more than just technology - it's my way of finding creative
-            solutions to real problems."
-          </li>
-        </ol>
+    <div
+      className={`${montserrat.variable} font-sans grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
+    >
+      <nav className="flex justify-between items-center w-full">
+        <ThemeToggle />
+      </nav>
+      <main className="flex flex-col sm:flex-row gap-8 items-center sm:justify-between w-full">
+        <div className="flex flex-col gap-8 sm:items-start">
+          <h1
+            className="text-3xl sm:text-4xl text-center sm:text-left font-bold"
+            style={{ color: "var(--geist-foreground)" }}
+          >
+            Junior Webdeveloper
+          </h1>
+          <ol className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            <li className="mb-2">
+              "Code is more than just technology - it's my way of finding
+              creative solutions to real problems."
+            </li>
+          </ol>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex items-center justify-center">
           <Image
             className="dark:invert"
-            src="/image.png "
+            src="/image.png"
             alt="image"
-            width={100}
-            height={100}
+            width={400}
+            height={500}
           />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+      <footer className="flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
